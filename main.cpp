1c9ad7614bc65d2cb183f289bc8ccda96252a832
kaustubh-765 <kaustubhmishra1221@gmail.com>
2022-02-10 08:29:59 +0530

    This is a bit of brief check

--- main.cpp
+++ main.cpp
@@ -11,12 +11,62 @@ int main(){
 	#endif
 //-------------------------------------------------------------//
 
+	map<string,int> freqlike, freqdisl;
+	set<string> list, like, disklike;
+
+	int customer;
+	cin>>customer;
+
+	while(customer--){
+		int x,l;
+
+		cin>>x;
+
+		for (int i=1; i<=x; i++){
+			char a[30];
+			cin>>a;
+
+			list.insert(a);
+			like.insert(a);
+
+			freqlike[a]++;
+		}
+
+		cin>>l;
+
+		for (int i=1; i<=l; i++){
+			char a[30];
+			cin>>a;
+
+			list.insert(a);
+			disklike.insert(a);
+			freqdisl[a]++;
+		}
+
+	}
+
+	/*for (auto i:list){
+		cout<<i<<" ";
+	}
+	cout<<endl;
+	for (auto i:like)
+		cout<<i<<" ";
+
+	cout<<endl;
+
+	for (auto i:disklike){
+		cout<<i<<" ";
+	}
+*/
+
+	for (auto i: freqlike)
+			cout<<i.first<<"-"<<i.second<<"    ";
 
 
-	int n;
-	cin>>n;
+	cout<<endl;
 
-	cout<<"Hello World"<<n;
+	for (auto i: freqdisl)
+			cout<<i.first<<"-"<<i.second<<"    ";
 
 	return 0;
 }
