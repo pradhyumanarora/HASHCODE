5a4ee7ad87ead8c06b0057e6ad1807df33ad4583
kaustubh-765 <kaustubhmishra1221@gmail.com>
2022-02-11 09:03:10 +0530

    This is a bit better output

---
+++ Kaustubh/Preety much it/test2.cpp
@@ -0,0 +1,31 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+
+
+int main(){
+//-------------------------------------------------------------//
+	#ifndef ONLINE_JUDGE
+		freopen("input.txt", "r", stdin);
+		freopen("output.txt", "w", stdout);
+	#endif
+//-------------------------------------------------------------//
+
+	int customer;
+	cin>>customer;
+
+	set<string> list, dislike;
+
+	while(customer--){
+
+		
+
+	}
+
+
+	cout<<final.size()<<" ";
+	for (auto i:final)
+		cout<<i<<" ";
+
+	return 0;
+}
